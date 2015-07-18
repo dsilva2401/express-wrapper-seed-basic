96fa710e934777d8f9c69f11651e74d729d59f42
Diego Silva <dsilva2401@gmail.com>
2015-07-17 21:14:02 -0500

    App settings dir created

---
+++ src/settings/index.js
@@ -0,0 +1,39 @@
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var compress = require('compression');
+var logger = require('morgan');
+
+
+module.exports = function ($app, $config, $express) {
+
+	// Set parsers
+	$app.use(bodyParser.json());
+	$app.use(bodyParser.urlencoded({ extended: true }));
+	$app.use(cookieParser());
+
+	// Set logger
+	$app.use(logger('dev'));
+
+	// Compress all requests
+	$app.use(compress());
+
+	// Set static folder
+	$app.use($express.static($config.publicDir));
+
+	// Set Access Control
+	$app.use(function (req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+		if ('OPTIONS' == req.method) res.send(200);
+		else next();
+	});
+
+	// Not found page
+	$app.use(function (req, res, next) {
+		res.status(404);
+		res.end('Not found :(');
+	});
+
+}
